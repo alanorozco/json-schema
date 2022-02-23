97091a285d2025fff8498ad9dd319e612b7931af
Alan Orozco <alan@orozco.xyz>
2022-02-23 11:02:01 -0800

    allow empty

--- src/validators.ts
+++ src/validators.ts
@@ -8,6 +8,7 @@ export const validateIsInteger = (
   name: string,
   value: any
 ): ValidationResult => {
+  if (value == null) return;
   if (typeof value !== "number" || Math.floor(value) !== value) {
     return [`${name} must be an integer`];
   }
@@ -23,6 +24,7 @@ export const validateIsRequired = (
 };
 
 export const validateNumber = (name: string, value: any): ValidationResult => {
+  if (value == null) return;
   if (typeof value !== "number") {
     return [`${name} must be a number`];
   }
@@ -33,12 +35,14 @@ export const validateStringLength = (
   value: any,
   length: number = 100
 ): ValidationResult => {
+  if (value == null) return;
   if (typeof value !== "string" || value.length > length) {
     return [`${name} must be a string under ${length} characters long`];
   }
 };
 
 export const validateUrl = (name: string, value: string) => {
+  if (value == null) return;
   if (typeof value !== "string" || !value.startsWith("https://")) {
     return [`${name} must be a valid URL starting with https://`];
   }
