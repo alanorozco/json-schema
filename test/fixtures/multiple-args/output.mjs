d83cf93d0c468fbd72c530e743219a04de173230
Alan Orozco <alan@orozco.xyz>
2022-02-23 12:23:21 -0800

    length test

--- test/fixtures/multiple-args/output.mjs
+++ test/fixtures/multiple-args/output.mjs
@@ -3,11 +3,12 @@ import {
 validateStringLength
 } from '../../../dist/runtime.mjs';
 
-const validateStringLength_0 = (obj, prop) => validateStringLength(obj, prop, 200);
-const validateStringLength_1 = (obj, prop) => validateStringLength(obj, prop, 100);
+const validateStringLength_0 = (obj, prop) => validateStringLength(obj, prop, 10);
+const validateStringLength_1 = (obj, prop) => validateStringLength(obj, prop, 5);
+const validateStringLength_2 = (obj, prop) => validateStringLength(obj, prop, 3);
 
 export default (obj) => merge([
 validate(obj, 'foo', [validateStringLength_0]),
 validate(obj, 'bar', [validateStringLength_1]),
-validate(obj, 'baz', [validateStringLength_1]),
+validate(obj, 'baz', [validateStringLength_2]),
 ]);
